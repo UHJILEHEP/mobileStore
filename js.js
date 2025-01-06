9564d4cfc3ef00d47bae7da1c4d386d71298abae
UHJILEHEP <pa6o4uua9pec@gmail.com>
2025-01-06 03:23:40 +0200

    update to v0.2.2

--- js.js
+++ js.js
@@ -1,4 +1,7 @@
 const itemsDiv = document.getElementById('items')
+const menu = document.getElementById('menu')
+const list = document.getElementById('list')
+let menuFlag = false
 
 const url = 'https://webmiddle-2e4b.restdb.io/rest/products'
 const apiKey = '6779d251f0549d1bfa8d2fa6'
@@ -19,12 +22,11 @@ fetch(url,{
     itemsDiv.innerHTML = ''
     console.log(result)
     result.forEach(element => {
-        // console.log("el")
         itemsDiv.innerHTML += `
                                 <div class="item" id="item">
                                     <img class="itemImg" src="${element.photo_url}" alt="Item image">
                                     <h2>${element.title}</h2>
-                                    <p>${element.description}</p>
+                                    <p class="itemDescription">${element.description}</p>
                                     <div class="itemFooter">
                                         <p class="itemPrice">${element.price}</p>
                                         <div class="buyButton" id="buyButton" onclick="buy()">Buy</div>
@@ -34,3 +36,17 @@ fetch(url,{
     });
     
 })
+
+
+
+function buy(){
+    alert('Coming soon with updates...')
+}
+
+function menuf(){
+    alert('Coming soon with updates...')
+    // menuFlag = !menuFlag
+    // console.log(list.style.visibility)
+    // if(menuFlag) list.style.visibility = 'visible'
+    // else list.style.visibility = 'hidden'
+}
